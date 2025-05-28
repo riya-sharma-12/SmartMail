471eead324ccedb58dee6a2ac6f92d982f5bd451
riya-sharma-12 <riyas.sharma124@gmail.com>
2025-05-28 16:05:28 +0530

    c

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use('/api/emails', emailRoutes);
 
 app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+  console.log('Server running on http://localhost:3000 --');
 
   // Step 1: Start email fetching
   // emailController();
